149b54a7962c3908571d4bd873c833bc3c01e1d3
Tom <thomasdhkim91@gmail.com>
2022-03-07 14:01:24 -0500

    partial content and test cases

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project Setup
+
+[![Production Workflow](https://github.com/tkim91/IS601-Project1/actions/workflows/prod.yml/badge.svg)](https://github.com/tkim91/IS601-Project1/actions/workflows/prod.yml)
+
+* [Production Deployment](https://tkim91-project1-prod.herokuapp.com/)
+
+
+[![Development Workflow](https://github.com/tkim91/IS601-Project1/actions/workflows/dev.yml/badge.svg)](https://github.com/tkim91/IS601-Project1/actions/workflows/dev.yml)
+
+* [Developmental Deployment](https://tkim91-project1-dev.herokuapp.com/)
+
+
+## Running Locally
+
+1. To Build with docker compose:
+   docker compose up --build
+2. To run tests, Lint, and Coverage report use this command: pytest --pylint --cov
+
+.pylintrc is the config for pylint, .coveragerc is the config for coverage and setup.py is a config file for pytest
---
+++ tests/pages_test.py
@@ -0,0 +1,37 @@
+"""This test the homepage"""
+
+
+def test_request_home(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_request_cicd(client):
+    """This makes the CICD page"""
+    response = client.get("/cicd")
+    assert response.status_code == 200
+
+
+def test_request_docker(client):
+    """This makes the Docker page"""
+    response = client.get("/docker")
+    assert response.status_code == 200
+
+
+def test_request_flask(client):
+    """This makes the Docker page"""
+    response = client.get("/flask")
+    assert response.status_code == 200
+
+
+def test_request_git(client):
+    """This makes the Github page"""
+    response = client.get("/git")
+    assert response.status_code == 200
+
+
+def test_request_page_not_found(client):
+    """This checks for a non-hosted page"""
+    response = client.get("/page5")
+    assert response.status_code == 404
